3df98a5f0724491ee9a75f179dcfedd4615fe03d
Gustavo C R da Silva <86523009+Gustavocrs@users.noreply.github.com>
2025-05-31 01:36:43 -0300

    Atualizar o page.jsx

--- src/app/principal/page.jsx
+++ src/app/principal/page.jsx
@@ -16,28 +16,28 @@ export default function Principal() {
       <NavBar user={user} />
       <div className="ml-16 mt-14">
         <div className="flex flex-col items-center justify-center h-full">
-Bem-vindo ao Sistema de Gestão Escolar
+<h1>Bem-vindo ao Sistema de Gestão Escolar</h1>
 
-Organize, registre e acompanhe as atividades escolares com mais eficiência.
+<h2>Organize, registre e acompanhe as atividades escolares com mais eficiência.</h2>
 
-Nosso sistema foi desenvolvido para facilitar o dia a dia de escolas, oferecendo uma plataforma simples e intuitiva para o gerenciamento de ocorrências disciplinares, cadastros de advertências, controle de alunos e outras funcionalidades administrativas.
+<p>Nosso sistema foi desenvolvido para facilitar o dia a dia de escolas, oferecendo uma plataforma simples e intuitiva para o gerenciamento de ocorrências disciplinares, cadastros de advertências, controle de alunos e outras funcionalidades administrativas.</p>
 
-Principais funcionalidades:
+<h2>Principais funcionalidades:</h2>
 
-📌 Cadastro e acompanhamento de advertências disciplinares
-Registre ocorrências com detalhes, acompanhe o histórico do aluno e gere relatórios com facilidade.
+<p>📌 Cadastro e acompanhamento de advertências disciplinares</p>
+<p>Registre ocorrências com detalhes, acompanhe o histórico do aluno e gere relatórios com facilidade.</p>
 
-🧑‍🎓 Gerenciamento de alunos
-Mantenha informações atualizadas de cada estudante, com acesso rápido a dados importantes.
+<p>🧑‍🎓 Gerenciamento de alunos</p>
+Mantenha informações atualizadas de cada estudante, com acesso rápido a dados importantes.</p>
 
-🗂️ Organização de turmas e disciplinas
-Visualize e edite a estrutura da escola de forma prática, com controle completo sobre turmas, séries e professores.
+<p>🗂️ Organização de turmas e disciplinas</p>
+<p>Visualize e edite a estrutura da escola de forma prática, com controle completo sobre turmas, séries e professores.</p>
 
-📝 Relatórios e histórico disciplinar
-Gere relatórios completos para reuniões pedagógicas, conselhos escolares ou comunicação com os responsáveis.
+<p>📝 Relatórios e histórico disciplinar</p>
+<p>Gere relatórios completos para reuniões pedagógicas, conselhos escolares ou comunicação com os responsáveis.</p>
 
-🔐 Acesso seguro por perfil de usuário
-Controle o que cada colaborador pode visualizar ou editar no sistema, garantindo segurança e responsabilidade.
+<p>🔐 Acesso seguro por perfil de usuário</p>
+<p>Controle o que cada colaborador pode visualizar ou editar no sistema, garantindo segurança e responsabilidade.</p>
 </div>
       </div>
     </div>
