0c7d9e2d1d503439fe649127034f2a62a7f33602
Gustavo C R da Silva <86523009+Gustavocrs@users.noreply.github.com>
2025-06-11 16:13:19 -0300

    update

--- src/components/withAuth.jsx
+++ src/components/withAuth.jsx
@@ -1,17 +1,31 @@
 "use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import {useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
+import {CircularProgress} from "@mui/material";
 
 export default function withAuth(Component) {
   return function ProtectedComponent(props) {
     const router = useRouter();
+    const [isAuth, setIsAuth] = useState(null);
 
     useEffect(() => {
-      const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+      const token =
+        typeof window !== "undefined" ? localStorage.getItem("token") : null;
       if (!token) {
         router.push("/");
+        setIsAuth(false);
+      } else {
+        setIsAuth(true);
       }
     }, [router]);
+
+    if (isAuth === null) {
+      return (
+        <div className="w-full h-full flex justify-center items-center">
+          <CircularProgress />
+        </div>
+      );
+    }
 
     return <Component {...props} />;
   };
